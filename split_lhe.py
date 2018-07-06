5ba4a7a5b1fe2709bb62dbd00bff8942d2f1e6b2
Duncan Adams <duncan.adams@protonmail.ch>
2018-07-06 00:34:34 -0400

    implemented overflow handling

--- split_lhe.py
+++ split_lhe.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python3
 ###### TODO ######
 ## Figure out header stuff
-## Handle potential Overflow events
 ## Write number of events written in header
 ## Write LHE version info at top
 ## add checks for bad inputs
 ## clean code
-
+###################
 
 import sys, argparse, os.path, time
 import xml.etree.ElementTree as ET
@@ -29,16 +29,36 @@ init = root.find('init')
 
 #For every n_events, make a file
 no_files = n_evts/evts_per_file
+extra_evts = n_evts % evts_per_file
+
 evt_i = 0
 events = root.findall('event')
 
 for f_i in range(int(no_files)):
     out_file = open('out_lhe_' + str(f_i) + '.lhe', 'w')
-    # ~ out_file.write(ET.tostring(header).decode('utf-8'))
-    out_file.write(ET.tostring(init).decode('utf-8'))
     
+    if args.keep_header:
+        out_file.write(ET.tostring(header).decode('utf-8'))
+    out_file.write(ET.tostring(init).decode('utf-8'))
+
     for i in range(evts_per_file):
         out_file.write(ET.tostring(events[i + f_i*evts_per_file]).decode('utf-8'))
+
+    out_file.close()
+
+
+if extra_evts > 0:
+    #loop counters don't go out of scope in python :)
+    f_i += 1
+
+    out_file = open('out_lhe_' + str(f_i) + '.lhe', 'w')
     
-  
-out_file.close()
+    if args.keep_header:
+        out_file.write(ET.tostring(header).decode('utf-8'))
+    out_file.write(ET.tostring(init).decode('utf-8'))
+
+    for i in range(int(extra_evts)):
+        out_file.write(ET.tostring(events[i + f_i*evts_per_file]).decode('utf-8'))
+
+    out_file.close()
+
